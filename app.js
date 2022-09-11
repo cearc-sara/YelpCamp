64b02683b1538c2d8bcf58a84da71508cdb197c9
Sara Cearc <scearc930@gmail.com>
2022-09-11 13:46:47 -0400

    added index of campgrounds

--- app.js
+++ app.js
@@ -23,10 +23,13 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'Cheap Camping!'})
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    res.render('campgrounds/show')
 })
 
 app.listen(3000, () => {

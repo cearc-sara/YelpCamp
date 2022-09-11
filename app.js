97b5db1fa0ff0f39bcf0bb7700078d307408e8e1
Sara Cearc <scearc930@gmail.com>
2022-09-11 14:20:46 -0400

    added functionality for adding a new campground

--- app.js
+++ app.js
@@ -19,18 +19,34 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const camp = new Campground(req.body.campground);
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
-    res.render('campgrounds/show')
-})
+    const {id} = req.params;
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/show', {campground});
+});
+
+
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000')

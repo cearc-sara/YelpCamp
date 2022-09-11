8c834cbb1d7393a699d12d2cc4ca2a9ad15299a9
Sara Cearc <scearc930@gmail.com>
2022-09-11 15:55:28 -0400

    added functionality for deleting a campground

--- app.js
+++ app.js
@@ -58,6 +58,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
 })
 
 
